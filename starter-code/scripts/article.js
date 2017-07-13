d1813a706a6e955450aca0a6c9b0a6801c6d322a
Chuck Forsythe <thecforsythe@gmail.com>
2017-07-13 13:26:46 -0500

    created render function

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,7 +12,10 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+var template = $('article-template').html();
+var templateRender = Handlebars.compile(template);
+return templateRender(this);
+};
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
