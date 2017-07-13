2d735a30bb7d64aad423af96734ae97fd2a5bc1a
nerdbetter <jasontlogan@gmail.com>
2017-07-13 14:59:33 -0500

    added some style to make the page look not awful

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,20 +12,19 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-var template = $('article-template').html();
-var templateRender = Handlebars.compile(template);
-return templateRender(this);
+  var template = $('#article-template').html();
+  var templateRender = Handlebars.compile(template);
+  return templateRender(this);
 };
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+// TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
 
-};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
