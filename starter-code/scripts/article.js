cb24a4f05b134960eee5b8bae3214002340368b4
nerdbetter <jasontlogan@gmail.com>
2017-07-13 15:10:16 -0500

    fixed stupid linter spacing on comments

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,10 +16,10 @@ Article.prototype.toHtml = function() {
   var templateRender = Handlebars.compile(template);
   return templateRender(this);
 };
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
-  //   Since your template can't hold any JS logic, we need to execute the logic here.
-  //   The result is added to the object as a new property, which can then be referenced by key in the template.
-  //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+// REVIEW: If your template will use properties that aren't on the object yet, add them.
+//   Since your template can't hold any JS logic, we need to execute the logic here.
+//   The result is added to the object as a new property, which can then be referenced by key in the template.
+//   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
 this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
